8b67a826380224f54ab98954016875f8ee2513ce
Cory Wilson <cory@coryw.net>
2023-12-09 11:07:53 -0500

    Day 2 Part 1

---
+++ day2/main.ps1
@@ -0,0 +1,31 @@
+$red = 12
+$green = 13
+$blue = 14
+$colors  = @('red', 'green', 'blue')
+$file = Get-Content -Path "input.txt"
+
+$sum = 0
+
+for ($i = 0; $i -lt $file.Length; $i++)
+{
+    $gameIdToAdd = $i + 1
+    foreach ($color in $colors)
+    {
+        $colorMax = (Get-Variable $color).Value
+        $numbers = $file[$i] 
+            | Select-String -AllMatches -Pattern "\d+(?= $color)" 
+            | Select-Object -ExpandProperty Matches
+            | ForEach-Object { [int]$_.Value }
+        foreach ($number in $numbers)
+        {
+            if ($number -gt $colorMax)
+            {
+                $gameIdToAdd = 0
+            }
+        }
+    }
+    $sum += $gameIdToAdd
+}
+
+echo "Final sum: $sum"
+
